11baca47e2ce8b9b17280164afcb92ae88876c65
James Junior <jamescosjr@gmail.com>
2024-10-06 16:57:04 -0300

    feat: build the repository
    Co-authored-by: Whyska <WhysKJunior@users.noreply.github.com>
    Co-authored-by: Camile Victória <camileoliveira@users.noreply.github.com>
    Co-authored-by: Laís Lima <lis-lais@users.noreply.github.com>

--- src/repository/bookRepository.js
+++ src/repository/bookRepository.js
@@ -1,18 +1,78 @@
-const idGenerator = require('../utils/idGenerator');
+const idGenerator = require("../utils/idGenerator");
 
 let books = [];
 
 function create(book) {
-    const  id = idGenerator();
-    const newBook = {id, ...book};
-    books.push(newBook);
-    return newBook;
+  const id = idGenerator();
+  const newBook = { id, ...book };
+  books.push(newBook);
+  return newBook;
 }
 
 function findAll() {
-    return books;
+  return books;
+}
+
+function updateBook(id, updatedBook) {
+  const index = books.findIndex((book) => book.id == id);
+  if (index !== -1) {
+    books[index] = { id, ...updatedBook };
+    return books[index];
+  }
+}
+
+function deleteBook(id) {
+  const index = books.findIndex((book) => book.id == id);
+  if (index !== -1) {
+    return books.splice(index, 1)[0];
+  }
+  return null;
+}
+
+function findByTitle(title) {
+  const booksFiltered = books.filter((book) => {
+    if (book.title == title) {
+      return book;
+    }
+  });
+  return booksFiltered;
+}
+
+function findByAuthor(author) {
+  const booksFiltered = books.filter((book) => {
+    if (book.author == author) {
+      return book;
+    }
+  });
+  return booksFiltered;
+}
+
+function findByYear(year) {
+  const booksFiltered = books.filter((book) => {
+    if (book.year == year) {
+      return book;
+    }
+  });
+  return booksFiltered;
+}
+
+function findByGenre(genre) {
+  const booksFiltered = books.filter((book) => {
+    if (book.genre == genre) {
+      return book;
+    }
+  });
+  return booksFiltered;
 }
 
 module.exports = {
-    create, books, findAll
-}
+  create,
+  books,
+  findAll,
+  deleteBook,
+  updateBook,
+  findByTitle,
+  findByAuthor,
+  findByYear,
+  findByGenre,
+};
