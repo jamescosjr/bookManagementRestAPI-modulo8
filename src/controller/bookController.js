9eb179dbc37776c06383560a48aa893963288d33
WashingtonJR <washington.junior.ibv@gmail.com>
2024-10-11 15:49:07 -0300

    adição de try catch no listBookHandler e no createBookHandler

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -2,21 +2,32 @@ const bookRepository = require("../repository/bookRepository");
 const validateBookData = require("../utils/validations");
 
 function createBookHandler(req, res) {
-  const book = req.body;
-  const validationResult = validateBookData(book);
+  try {
+    const book = req.body;
+    const validationResult = validateBookData(book);
 
-  if (validationResult !== true) {
-    return res.status(400).json({ message: validationResult });
+    if (validationResult !== true) {
+      return res.status(400).json({ message: validationResult });
+    }
+
+    const newBook = bookRepository.create(book);
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.error("Error creating book:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
-
-  const newBook = bookRepository.create(book);
-  res.status(201).json(newBook);
 }
 
 function listBookHandler(req, res) {
-  const books = bookRepository();
-  res.json(books)
+  try {
+    const books = bookRepository();
+    res.json(books);
+  } catch (error) {
+    console.error("Error listing books:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
+
 
 module.exports = {
   createBookHandler,
