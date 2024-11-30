f8460293b08b668d937e93e9589574b4efeb7bb3
jamescosjr <jamescosjr@gmail.com>
2024-11-30 09:53:01 -0300

    feat: add update and delete functionality to book repository

--- src/infrastructure/repositories/bookRepository.write.js
+++ src/infrastructure/repositories/bookRepository.write.js
@@ -1,6 +1,6 @@
-const book = require("../schema/bookSchema");
+const book = require('../schema/bookSchema.js');
 
-function bookRegister(title, author, year, genre){
+function bookRegister(title, author, year, genre) {
   try {
     const newBook = new book({
       title,
@@ -13,6 +13,28 @@ function bookRegister(title, author, year, genre){
     console.error("Error registering book:", error);
     throw error;
   }
-};
+}
 
-module.exports = bookRegister;
+async function updateBook(id, title, author, year, genre) {
+  try {
+    return await book.findByIdAndUpdate(
+      id,
+      { title, author, year, genre },
+      { new: true, lean: true }
+    );
+  } catch (error) {
+    console.error("Error updating book in repository:", error);
+    throw error;
+  }
+}
+
+async function deleteBook(id) {
+  try {
+    return await book.findByIdAndDelete(id);
+  } catch (error) {
+    console.error("Error deleting book in repository:", error);
+    throw error;
+  }
+}
+
+module.exports = { bookRegister, updateBook, deleteBook };
