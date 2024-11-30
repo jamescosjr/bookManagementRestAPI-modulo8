a3835dea39ccdb789a509ee24758f5b4fe826685
jamescosjr <jamescosjr@gmail.com>
2024-11-30 10:43:57 -0300

    feat: add routes to retrieve books by title, author, year, and genre

--- src/application/controller/routes.js
+++ src/application/controller/routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { bookRegister, listAllBooks, updateBook, deleteBook } = require("./bookController");
+const { bookRegister, listAllBooks, updateBook, deleteBook, getBookByTitle, getBookByAuthor, getBookByYear, getBookByGenre } = require("./bookController");
 
 const router = express.Router();
 
@@ -7,5 +7,9 @@ router.post("/books", bookRegister);
 router.get("/books", listAllBooks);
 router.put("/books/:id", updateBook);
 router.delete("/books/:id", deleteBook);
+router.get("/books/title/:title", getBookByTitle);
+router.get("/books/author/:author", getBookByAuthor);
+router.get("/books/year/:year", getBookByYear);
+router.get("/books/genre/:genre", getBookByGenre);
 
 module.exports = router;
