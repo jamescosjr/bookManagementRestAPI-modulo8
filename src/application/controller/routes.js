52496d953a720381507635017dc2e3bc993a56cb
jamescosjr <jamescosjr@gmail.com>
2024-11-30 09:53:13 -0300

    feat: add update and delete routes for book management

--- src/application/controller/routes.js
+++ src/application/controller/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { bookRegister, listAllBooks } = require("./bookController");
+const { bookRegister, listAllBooks, updateBook, deleteBook } = require("./bookController");
 
 const router = express.Router();
 
 router.post("/books", bookRegister);
 router.get("/books", listAllBooks);
+router.put("/books/:id", updateBook);
+router.delete("/books/:id", deleteBook);
 
 module.exports = router;
