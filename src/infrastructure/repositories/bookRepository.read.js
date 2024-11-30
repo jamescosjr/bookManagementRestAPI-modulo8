5c07a4b430a20c3f07d90f59118ee0f5c39b11ad
jamescosjr <jamescosjr@gmail.com>
2024-11-30 10:43:46 -0300

    feat: add functions to retrieve books by title, author, year, and genre

--- src/infrastructure/repositories/bookRepository.read.js
+++ src/infrastructure/repositories/bookRepository.read.js
@@ -9,4 +9,48 @@ function getAllBooks() {
     }
 }
 
-module.exports = getAllBooks;
+async function getBookByTitle(title) {
+    try {
+        return Book
+            .find({ title });
+    }
+    catch (error) {
+        console.error("Error getting books:", error);
+        throw error;
+    }
+}
+
+async function getByAuthor(author) {
+    try {
+        return Book
+            .find({ author });
+    }
+    catch (error) {
+        console.error("Error getting books:", error);
+        throw error;
+    }
+}
+
+async function getByYear(year) {
+    try {
+        return Book
+            .find({ year });
+    }
+    catch (error) {
+        console.error("Error getting books:", error);
+        throw error;
+    }
+}
+
+async function getByGenre(genre) {
+    try {
+        return Book
+            .find({ genre });
+    }
+    catch (error) {
+        console.error("Error getting books:", error);
+        throw error;
+    }
+}
+
+module.exports = {getAllBooks, getBookByTitle, getByAuthor, getByYear, getByGenre};
